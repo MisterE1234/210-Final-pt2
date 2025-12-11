7fdbb4ccf7df27aa11c40d25616bcf68d6f4bcdc
MisterE1234 <MisterE1234@outlook.com>
2025-12-11 13:31:51 -0800

    3rd Commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-
+bool const debug = true; int const INITIAL_CUST = 3;
 
 struct CoffeeLine{
     string name = "N/A";
@@ -15,7 +15,32 @@ struct CoffeeLine{
 };
 
 int main (){
+    srand(time(0));
     string custNames[20] = {"Ethan", "Alice", "Bob", "Cathy", "David", "Eva", "Frank", "Grace", "Hannah", "Ian",
                              "Jack", "Kathy", "Liam", "Mia", "Noah", "Olivia", "Paul", "Quinn", "Rachel", "Sam"};
+    string custDrinks[20] = {"Latte", "Cappuccino", "Espresso", "Mocha", "Americano", "Macchiato", "Flat White", "Cortado", "Ristretto", "Affogato",
+                             "Irish Coffee", "Iced Coffee", "Cold Brew", "Frappuccino","Hot Chocolate", "Chai Latte", "Turkish Coffee", "Vienna Coffee", "Doppio", "Red Eye"};
+    CoffeeLine* head = nullptr;
     
+    for (int i = 0; i < INITIAL_CUST; i++){
+        CoffeeLine* newCust = new CoffeeLine;
+        newCust->name = custNames[rand()%20];
+        newCust->drink = custDrinks[rand()%20];
+        newCust->next = head;
+
+        if(debug){
+            cout << "new customer: " << newCust->name << ", order: " << newCust->drink << endl;
+        }
+    }
+
+CoffeeLine* current = head;
+    
+    while(current != nullptr){
+
+    }
+
+}
+
+
+    return 0;
 }
