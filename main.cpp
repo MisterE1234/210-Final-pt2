86a8fb93113245ae58b1a9e4d057096616567fe4
MisterE1234 <MisterE1234@outlook.com>
2025-12-11 13:20:42 -0800

    2nd Commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,21 @@
 //COMSC-210|Ethan Aylard| Final-pt2
 //IDE used : Visual Studio Code
+
+#include <iostream>
+
+using namespace std;
+
+
+
+struct CoffeeLine{
+    string name = "N/A";
+    string drink = "N/A";
+    CoffeeLine* next = nullptr;
+
+};
+
+int main (){
+    string custNames[20] = {"Ethan", "Alice", "Bob", "Cathy", "David", "Eva", "Frank", "Grace", "Hannah", "Ian",
+                             "Jack", "Kathy", "Liam", "Mia", "Noah", "Olivia", "Paul", "Quinn", "Rachel", "Sam"};
+    
+}
